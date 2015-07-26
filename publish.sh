b969a9a73f70fe467c776ad75ac9546c7b50d51f
sainaen <sainaen@gmail.com>
2015-07-26 12:02:40 +0300

    Update publishing machinery

--- publish.sh
+++ publish.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 set -e # stop at any error
 
+repoUrl="https://github.com/sainaen/gp-helper.git"
+repoFolder="gp-helper-site"
+buildFolder="build"
+
+git clone --branch gh-pages $repoUrl $repoFolder
+rm -rf $repoFolder/*
+cp -r $buildFolder/* $repoFolder/
+
+cd $repoFolder
+
 # trick to hide GitHub token in logs
 git config credential.helper "store --file=.git/credentials"
 echo "https://$GH_TOKEN:@github.com" > .git/credentials
 
+# commit changes
+git add --all
+git commit --author="Travis-CI bot <bot@travis-ci.com>" --message="Update the site"
+
 # push latest commit in current branch to `gh-pages`
-git push "https://github.com/sainaen/gp-helper.git" HEAD:gh-pages
+git push $repoUrl HEAD:gh-pages
